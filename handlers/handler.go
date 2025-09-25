5655ca1e557f28c8bf268c88ced1513ee3ec2047
Leandro Villalba <villalbaleandrogabriel@gmail.com>
2025-09-25 09:48:18 -0300

    renderizado y modularidad de funciones

--- handlers/handler.go
+++ handlers/handler.go
@@ -3,23 +3,18 @@ package handlers
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+)
+
+const (
+	templateDir  = "templates/"
+	baseTemplate = templateDir + "base.html"
 )
 
 // IndexHandler handles requests to the root URL path. It responds with a simple "Hello, World!" message.
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/base.html", "templates/index.html")
-	if err != nil {
-		http.Error(w, "Error loading template", http.StatusInternalServerError)
-		return
-	}
-	
-	err = tmpl.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		http.Error(w, "Error rendering template", http.StatusInternalServerError)
-		return
-	}
-
+	renderTemplate(w, baseTemplate, "index.html", nil)
 }
 
 // NewGame handles requests to the /new-game URL path. It responds with a "New Game!" message.
@@ -39,4 +34,15 @@ func Play(w http.ResponseWriter, r *http.Request) {
 // SaveGame handles requests to the /save-game URL path. It responds with a "Save Game!" message.
 func About(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "About!")
+}
+
+func renderTemplate(w http.ResponseWriter, base, page string, data any) {
+	tmpl := template.Must(template.ParseFiles(base, templateDir + page))
+
+	err := tmpl.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		http.Error(w, "Error rendering template", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
 }
